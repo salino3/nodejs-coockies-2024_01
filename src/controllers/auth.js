f21b69318a72825d0906dd31324858e49ff73117
Salino3 <flaviobcn012@gmail.com>
2024-12-20 09:53:05 +0100

    register user after validations

--- src/controllers/auth.js
+++ src/controllers/auth.js
@@ -1,6 +1,43 @@
-exports.register = (req, res) => {
-  // Implement the registration logic here
-  console.log("Res: ", req.body);
+const { db } = require("../../db");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcryptjs");
 
-  res.send("Form Submitted!");
+exports.register = async (req, res) => {
+  const { name, email, password, passwordConfirm } = req.body;
+
+  await db.query(
+    "SELECT email FROM users WHERE email = (?)",
+    [email],
+    async (err, result) => {
+      if (err) {
+        console.error(err);
+        return;
+      } else {
+        if (result.length > 0) {
+          return res.render("register", {
+            message: "That email is already in use",
+          });
+        } else if (password !== passwordConfirm) {
+          return res.render("register", {
+            message: "Passwords do not match",
+          });
+        }
+        const hashedPassword = await bcrypt.hashSync(password, 10);
+        db.query(
+          "INSERT INTO users SET ?",
+          { name, email, password: hashedPassword },
+          (err, results) => {
+            if (err) {
+              console.error(err);
+              return;
+            } else {
+              return res.render("register", {
+                message: "User registered",
+              });
+            }
+          }
+        );
+      }
+    }
+  );
 };
